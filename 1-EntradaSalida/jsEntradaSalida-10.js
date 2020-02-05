39b4eea67ad1893f53d0d10e4e6a0f46c6016491
FernandezRodrigo373 <60651866+FernandezRodrigo373@users.noreply.github.com>
2020-02-05 12:09:51 -0300

    10 resuelto
    10 faltan cosas

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,8 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	//importe descuento resultado
+	//500       20          400
+	//2000      40          1200
 	
 }
