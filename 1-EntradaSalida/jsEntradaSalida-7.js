580795ca38ea32091c41fcaa08942caa3cc05e77
FernandezRodrigo373 <60651866+FernandezRodrigo373@users.noreply.github.com>
2020-02-05 10:44:14 -0300

    ejercicio 7 resuelto
    probando si funciona

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,7 +3,28 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+	
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+	console.log("el numero es : "+primerNumero);
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+	console.log("el numero es : "+segundoNumero);
+
+	resultado=primerNumero+segundoNumero;
+	console.log(resultado);
+
+	 //primer linea de codigo pongo brekpoint 
+	 //+-*/ F11
+
+
+
+
 	
 }
 
