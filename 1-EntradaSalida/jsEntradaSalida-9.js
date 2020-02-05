7b37a5a8306544cbe9ee896bb94bf6b1909e83ad
FernandezRodrigo373 <60651866+FernandezRodrigo373@users.noreply.github.com>
2020-02-05 11:57:21 -0300

    9 resuleto
    sin errores

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,35 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero=document.getElementById('sueldo').value;
+	primerNumero=parseInt(primerNumero);
+	console.log(" el numero es "+primerNumero);
+
+	document.getElementById('resultado').value=segundoNumero;
+	segundoNumero=parseInt(segundoNumero);
+	
+
+	resultado=primerNumero+primerNumero*10/100;
+	console.log(" el numero es "+ resultado);
+	console.log(resultado);
+
+	//precio final=precio*21/100+precio
+	//precio con descuento=precio*porcentaje de descuento/100
+	//precio con descuento= precio - precio*porecentaje deccuento/100
+	//precio con impuestos=precio+precio*aumento/100
+	
+
+
+
+
+
+
+
+
 
 	
 }
