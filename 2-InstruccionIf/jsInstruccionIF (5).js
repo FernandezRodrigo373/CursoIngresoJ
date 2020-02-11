7c4f494adb2c6f89262ee6765dae81677372f325
FernandezRodrigo373 <60651866+FernandezRodrigo373@users.noreply.github.com>
2020-02-11 12:14:27 -0300

    if+random
    hechos

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,5 +1,22 @@
 function mostrar()
 {
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
+	//if(edad>18&&edad<12) NO
+	if(edad>17)
+	{
+		alert(" no es adolescente");
+	}
+	if(edad<13)
+	{
+		alert(" no es adolescente");
+	}
+
+	
+	//if(!(edad>12&&edad<18))
+
 //tomo la edad  
 
 
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
 
+	if(edad>17)
+	{
+		alert(" es mayor de edad ");
+	}
+	else
+	{
+		if(edad<13)
+	
+		{
+			alert(" es menor ");
+		}
+		else
+		{
+			alert(" es adolescente ");
+		}
+	}
+		
 
-
-
-}//FIN DE LA FUNCIÓN
+	
+	}//FIN DE LA FUNCIÓN
