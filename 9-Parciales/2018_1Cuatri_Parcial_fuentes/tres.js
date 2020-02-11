1ac30b6091f2cb563b29deb9185763170e510fe0
FernandezRodrigo373 <60651866+FernandezRodrigo373@users.noreply.github.com>
2020-02-10 21:05:48 -0300

    parciales
    hechos

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+	var precio;
+	var porcentajeDeDescuento;
+	var precioFinal;
+
+	precio=prompt(" Indique el precio ");
+	porcentajeDeDescuento=prompt(" indique porcentaje de descuento ");
+
+	
+	precioFinal=precio-precio*porcentajeDeDescuento/100;
+
+	document.getElementById('elPrecioFinal').value=precioFinal;
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho=prompt(" Indique ancho del rectangulo (cm) ");
+	largo=prompt(" Indique largo del rectangulo (cm) ");
+
+	perimetro=2*ancho+2*largo;
+
+	alert(" El perimetro es "+perimetro+"cm");
+
 
 }
