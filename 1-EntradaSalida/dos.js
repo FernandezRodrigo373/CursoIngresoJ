f2ed50161a79bb568193890ae0f7b6070c625734
FernandezRodrigo373 <60651866+FernandezRodrigo373@users.noreply.github.com>
2020-02-10 21:02:46 -0300

    todos los ejercicios
    todo hecho

---
+++ 1-EntradaSalida/dos.js
@@ -0,0 +1,15 @@
+function mostrar()
+{
+	var nombre;
+	var localidad;
+
+	nombre=document.getElementById('elNombre').value;
+
+	alert(" usted es "+ nombre);
+
+	localidad=document.getElementById('laLocalidad').value;
+
+	alert(" vive en la localidad de "+localidad);
+
+  
+}
