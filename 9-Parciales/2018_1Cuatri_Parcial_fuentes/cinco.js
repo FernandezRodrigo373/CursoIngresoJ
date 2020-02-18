ee1490c8a2e730c569c2da9f711f0842c39865fc
FernandezRodrigo373 <60651866+FernandezRodrigo373@users.noreply.github.com>
2020-02-18 18:32:46 -0300

    tp planeta y hora (switch)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,27 @@
 function mostrar()
-{
 
+{
+	var planeta;
+
+	planeta=prompt("indique el planeta");
+
+	switch(planeta)
+	{
+		case "tierra":
+			alert("acá vivimos");
+			break;
+		case "mercurio":
+		case "venus":
+			alert("acá hace más calor");
+			break;
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+			alert("acá hace más frio");
+			break;
+		default:
+			alert("no es un planeta");	
+	}	
 }
